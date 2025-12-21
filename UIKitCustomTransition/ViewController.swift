59c8b08d320063c7b4b54e8d325288348398d4cd
Shingo Sato <gonseeit@gmail.com>
2025-12-21 17:23:24 +0900

    Initial Commit

---
+++ UIKitCustomTransition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitCustomTransition
+//
+//  Created by Shingo Sato on 2025/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
